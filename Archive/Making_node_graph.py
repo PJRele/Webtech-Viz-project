c797732e0096ab00e30dd516a7389e19c329f411
AsafKed <asafkedem97@gmail.com>
2019-05-06 11:56:58 +0200

    Made radial graph, deleted head ; in co-authorship, created archive
    Functioning radial graph in jupyter + normal python
    Deleted ; at the beginning of the co-authorship csv
    Archive folder with first trials

--- Archive/Making_node_graph.py
+++ Archive/Making_node_graph.py
@@ -13,21 +13,41 @@ import networkx as nx
 from holoviews import opts
 
 hv.extension('bokeh')
-
+""""
 # declare size and color
 defaults = dict(width=400, height=400, padding=0.1)
 hv.opts.defaults(
     opts.EdgePaths(**defaults), opts.Graph(**defaults), opts.Nodes(**defaults))
 colors = ['#000000']+hv.Cycle('Category20').values
-
-# input CSV file
-# check if csv doesn't start with a character, and delete it (i.e. delete starting ;) 
-df = pd.read_csv('Given CSVs/GephiMatrix_co-authorship.csv', header=0, delimiter=';')
-print(df.head())
 """
-df_edges = pd.read_csv('C:/Users/Asaf/Documents/TU Software/Y3/Q4/DBL HTI Webtevh/Given CSVs/GephiMatrix_co-authorship.csv', skiprows=[0])
-df_nodes = hv.Nodes(pd.read_csv('C:/Users/Asaf/Documents/TU Software/Y3/Q4/DBL HTI Webtevh/Given CSVs/GephiMatrix_co-authorship.csv', skiprows=[1:]))
+##################################
+# test
+Data = open('Given CSVs/GephiMatrix_co-authorship.csv', "r")
+next(Data, None)  # skip the first line in the input file
+Graphtype = nx.Graph()
+
+G = nx.parse_edgelist(Data, delimiter=',', create_using=Graphtype,
+                      nodetype=int, data=(('weight', float),))
+hv_G = hv.Graph.from_networkx(G, nx.layout.circular_layout).opts(tools=['hover'])
+#fig = hv.render(hv_G)
+#print('Figure: ', fig)
+hv.output(hv_G, fig='png')
+#############################
+
+"""
+# input CSV file
+df = pd.read_csv('Given CSVs/GephiMatrix_co-authorship.csv', header=0, delimiter=';')
+df_shape = df.shape
+print("df shape: ", df_shape)
+# 1053 nodes
+df_edges = pd.read_csv('Given CSVs/GephiMatrix_co-authorship.csv')
+print(df_edges)
+
+
+df_nodes = hv.Nodes(pd.read_csv('Given CSVs/GephiMatrix_co-authorship.csv', skiprows=[1:df_shape[1]]))
+
 hv_graph = hv.Graph((edges_df, fb_nodes), label='Facebook Circles')
+
 
 # the graph in networkx format
 G = nx.from_pandas_edgelist(Data, create_using=Graphtype)
---
+++ Node_data.py
@@ -0,0 +1,72 @@
+# Asaf 6/5/19
+import bokeh # 1.1.0
+import pandas as pd
+import networkx as nx
+import matplotlib.pyplot as plt
+import holoviews as hv
+from holoviews import opts
+hv.extension('bokeh','matplotlib')
+# hv.extension('bokeh')
+
+
+# TO DO: output location
+
+
+# read file
+
+df = pd.read_csv('Given CSVs/GephiMatrix_co-authorship.csv', delimiter=';')
+# sample node size 
+# len(df.columns) to get all
+N = 50
+
+# the nodes, with deleted ';' in head of file
+all_nodes = df.columns.tolist()
+
+# sample size edges + weights
+def make_edges(dframe, sample_size, weight_threshold):
+    edges = []
+    weights = []
+    nodes = dframe.columns.tolist()
+    for row in range (0, sample_size):
+        for col in range (0, sample_size):
+            if dframe.iat[row,col] > weight_threshold:
+                edges.append([nodes[row],nodes[col],dframe.iat[row,col]])
+                weights.append(dframe.iat[row,col])
+    return edges, weights
+
+
+sample_nodes = all_nodes[:N-1]
+# row, column, weight
+sample_edges, sample_weights = make_edges(df, N-1, 0)
+
+# create graph
+def make_network(nodes, edges):
+    graph=nx.DiGraph()
+    graph.add_nodes_from(nodes)
+    for edge in edges:
+        graph.add_edge(edge[0], edge[1], weight=float(edge[2]*5))
+    return graph
+
+# everything works until here
+
+G = make_network(sample_nodes, sample_edges)
+# TO DO : make the following into a test
+# print('edges ', G.edges(data=True))
+
+# TO DO : write about these plots in report :
+# plt.figure(1,figsize=(12,12))
+# nx.draw(G,with_labels=False,font_size=15,node_size=100, k=1, iterations=20)
+# plt.show()
+#print('edges ', G.edges(data=True))
+
+
+circular_layout = hv.Graph.from_networkx(G, nx.layout.circular_layout).relabel('Circular Layout').options(width=650, height=650).opts(xaxis=None, yaxis=None, padding=0.1)
+
+# TO DO : Add node and edge sizes
+
+# TO DO : create option to select nodes OR edges
+# TO DO : fix export name (output location)
+# def export_graph(obj):
+#     # Using matplotlib (check bokeh documentation instead)
+#     hv.save(obj, 'plot.svg', backend='matplotlib')
+
